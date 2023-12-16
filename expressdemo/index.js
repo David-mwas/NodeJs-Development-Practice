7d401a8e9699fa21f3e3cbac6c403d2dd71e7594
David-mwas <dmwas704@gmail.com>
2023-12-16 23:17:16 +0300

    DELETE :handle delete request

--- expressdemo/index.js
+++ expressdemo/index.js
@@ -50,6 +50,19 @@ app.put("/api/courses/:id", (req, res) => {
   res.send(course);
 });
 
+// DELETE request
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) {
+    res.status(404).send("The course with the given id was not found!");
+  }
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
+});
+
 // PORT
 const port = process.env.PORT || 3001;
 app.listen(port, () => {

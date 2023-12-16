1134e45baf484772637f0a115c42e79188c20f15
David-mwas <dmwas704@gmail.com>
2023-12-16 21:45:26 +0300

    Add :add mongoose and  nodemon globally

--- expressdemo/index.js
+++ expressdemo/index.js
@@ -1,14 +1,46 @@
-const app = require("express")();
-// const app = express();
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
+// GET request
 app.get("/api/courses", (req, res) => {
-  res.send([1, 23, 4, 5]);
+  res.send(courses);
 });
 
-app.listen(5000, () => {
-  console.log("Listening on port 5000...");
+// POST requst
+app.post("/api/courses", (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
+app.get("/api/posts/:year/:month", (req, res) => {
+  res.send(req.params);
+});
+
+app.get("/api/posts/:year/:month", (req, res) => {
+  res.send(req.query);
+});
+
+// PORT
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
 });
